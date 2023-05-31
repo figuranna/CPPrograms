16f181b22f7eb8849d3fed48289e90973f7f71f2
figuranna <101461379+figuranna@users.noreply.github.com>
2023-05-31 23:48:35 +0200

    Upload

---
+++ OOP/oop_struct.cpp
@@ -0,0 +1,24 @@
+// Class with no methods
+#include <iostream>
+
+using namespace std;
+
+// ***** Code for struct here *****
+
+struct Student
+{
+	int age;
+	string first_name;
+	string last_name;
+	int gpa;
+};
+
+int main()
+{
+	Student s;
+	
+	cin >> s.age >> s.first_name >> s.last_name >> s.gpa;
+	cout << "Age: " << s.age << " First Name: " << s.first_name << " Last Name: " << s.last_name << " GPA: " << s.gpa << endl;
+	
+	return 0;
+}
