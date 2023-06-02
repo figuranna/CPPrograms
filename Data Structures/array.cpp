77699b7013b0f1f31c0c3da9ee93a4e29a490f26
Anna Sértő <101461379+figuranna@users.noreply.github.com>
2023-06-02 21:30:14 +0200

    Update array.cpp

--- Data Structures/array.cpp
+++ Data Structures/array.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	// Declaring the array + initializing the elements of the array
+	int arr[5] = {1,2,3,4,5}; // the size of the array cannot be changed later on
+	
+	/*
+	
+	Another way to initalize the elements
+	
+		int arr[5];
+		
+		arr[0] = 1;
+		arr[1] = 2;
+		arr[2] = 3;
+		arr[3] = 4;
+		arr[4] = 5;
+	
+	*/
+	
+	// printing out the elements from the array
+	for(int i = 0; i < 5; i++)
+	{
+		cout << arr[i];
+	}
+
+	return 0;
+}
