baf07de0a26d02ec5351d8ba33753eb1e693a936
Anna Sértő <101461379+figuranna@users.noreply.github.com>
2023-06-02 23:31:43 +0200

    Create queue.cpp

---
+++ Data Structures/queue.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	// create queue
+	queue<int> q;
+	
+	// inserting elements at the end
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	q.push(4);
+	q.push(5);
+	
+	// functions
+	q.size(); 
+	q.front(); // gives you the first element
+	q.back(); // gives you the last element
+	
+	// print out queue's elements
+	while(!q.empty())
+	{
+		cout << q.front() << ", ";
+		q.pop(); // removes the element from the front of the queue
+	}
+	
+	return 0;
+}
